5491af89405060ee5895c46ac81bc1adf8e927ab
rencabral <rcabral@ucsb.edu>
2018-08-19 13:00:22 -0700

    added pt lobos and carmel bay

--- ReefCheckVsPISCO.R
+++ ReefCheckVsPISCO.R
@@ -16,13 +16,15 @@ library(leaflet)
 library(ggplot2)
 library(reshape)
 
+library(ggpubr)#for stat_compare_means()
+
 rm(list=ls(all=TRUE))
 library(data.table)
 
 
-PATH = "H:/Jenn Project 2017" #  #Put  name of file that has both CA and OR catch here
-dataIN = fread(paste(PATH,"SBC reserve data all.txt",sep="/"))
-#dataIN = fread("H:/Jenn Project 2017/SBC reserve data all.txt")
+#PATH = "H:/Jenn Project 2017" #  #Put  name of file that has both CA and OR catch here
+#dataIN = fread(paste(PATH,"SBC reserve data all.txt",sep="/"))
+dataIN = fread("H:/Jenn Project 2017/SBC reserve data all.txt") #this is already a calculated data of pisco and not the raw data!
 head(dataIN,3)
 table(dataIN$site)
 
@@ -66,8 +68,30 @@ points(coordSAMPLE$lat_wgs84,coordSAMPLE$lon_wgs84)
 lines(country.mp, axes=TRUE, col="gray")
 
 #PLOT ReefCheck coordinates - fish channel islands
-ReefcheckFISH<-read.csv(paste(PATH,"ReefCheck/RCCA_fish_data.csv",sep="/"))
+#ReefcheckFISH<-read.csv(paste(PATH,"ReefCheck/RCCA_fish_data.csv",sep="/"))
+ReefcheckFISH<-read.csv("H:/Jenn Project 2017/ReefCheck/RCCA_fish_data.csv")
 head(ReefcheckFISH)
+
+table(ReefcheckFISH$Species)
+# barred sand bass black and yellow      black perch   black rockfish       blacksmith    blue rockfish         bocaccio   brown rockfish 
+# 47101            47135            48719            47220            48575            49133            46786            46794 
+# cabezon   china rockfish  copper rockfish        garibaldi   giant sea bass  gopher rockfish   grass rockfish       horn shark 
+# 46804            46784            46846            97307             8332            47085            46796            46788 
+# kelp bass   kelp greenling    kelp rockfish          lingcod          opaleye       pile perch    rainbow perch   rock greenling 
+# 52309           124351            47858            46881            47152            47370            46928            46785 
+# rock wrasse  rubberlip perch            sargo         senorita        sheephead    striped perch         treefish vermilion/canary 
+# 142966            46905            46821            49469           143230            48195            93588            46822 
+# yellowtail/olive     yoy rockfish 
+# 47040            41553 
+#what are the species of interets?
+#barred sand bass - Paralabrax nebulifer
+#black perch - Embiotoca jacksoni
+#black rockfish - Sebastes melanops
+#blacksmith - Chromis punctipinnis
+
+
+
+
 
 plot(ReefcheckFISH$Lon,ReefcheckFISH$Lat)
 
@@ -94,6 +118,235 @@ leaflet() %>%
   addTiles() %>%
     addCircleMarkers(data = ReefCheckLocation, ~Lon, ~Lat, radius = 3, color = "red") %>%
     addCircleMarkers(data = coordSAMPLE, ~lat_wgs84, ~lon_wgs84, radius = 1, color = "blue")
+
+
+
+#REN's try (4/4/2018)-------------------------------------------------------
+head(ReefcheckFISH)
+table(ReefcheckFISH$Amount) #ok, there are no NAs
+#1. 
+#isolate the four channel islands --- remove other points
+ReefCheckSampleCoord<-matrix(c(
+  "Anacapa_SMCA", -119.4375, 34.014729, "Anacapa",
+  "Anacapa_SMR1", -119.371498, 34.015751, "Anacapa",
+  "Anacapa_SMR2", -119.368385, 34.016499, "Anacapa",
+  "Anacapa_SMR3", -119.362396, 34.017467, "Anacapa",
+  "Anacapa_OUT", -119.364197, 34.012634, "Anacapa",
+  "Scruz_Scorpion", -119.552299, 34.048515, "SCruz",
+  "Scruz_Out_Front1",  -119.809998, 34.055, "SCruz",
+  "Scruz_Out_Front2", -119.755997, 34.054161, "SCruz",
+  "Scruz_Out_Front3", -119.702499, 34.035648, "SCruz",
+  "Scruz_Out_Back1", -119.554398, 33.990665, "SCruz",
+  "Scruz_Out_Back2", -119.550499, 33.998798, "SCruz",
+  "SRosa_SPoint", -120.125, 33.895, "SRosa",
+  "SRosa_Out", -120.103401, 33.90155, "SRosa"), nrow=4)
+
+ReefCheckSampleCoordFin<-data.frame(t(ReefCheckSampleCoord))
+names(ReefCheckSampleCoordFin) <- c("site","Lon","Lat","Island")
+ReefCheckSampleCoordFin  
+
+#merge the reefchech fish and the coordinates of interest
+ReefcheckFISH_forAnalysis <- merge(ReefcheckFISH,ReefCheckSampleCoordFin,by=c("Lon","Lat"))  
+table(ReefcheckFISH_forAnalysis$site)
+head(ReefcheckFISH_forAnalysis)
+
+#Assign species categories and merge
+speciescategory<-matrix(c(
+  "barred sand bass", "Target",
+  "black and yellow", "Target",     
+  "black perch"   , "Target",
+  "black rockfish" , "Target",      
+  "blacksmith"    , "Non-target",
+  "blue rockfish", "Target",
+  "bocaccio"   , "Target",
+  "brown rockfish", "Target",
+  "cabezon", "Target",
+  "china rockfish", "Target",
+  "copper rockfish", "Target",
+  "garibaldi" , "Non-target",
+  "giant sea bass",  "Non-target", 
+  "gopher rockfish", "Target",
+  "grass rockfish", "Target",
+  "horn shark", "Non-target",
+  "kelp bass", "Target",
+  "kelp greenling", "Target", 
+  "kelp rockfish", "Target",
+  "lingcod", "Target",
+  "opaleye", "Non-target",
+  "pile perch", "Target",
+  "rainbow perch", "Non-target",
+  "rock greenling",  "Target",
+  "rock wrasse", "Non-target",
+  "rubberlip perch", "Target",
+  "sargo", "Non-target",
+  "senorita", "Non-target",
+  "sheephead", "Target",
+  "striped perch",  "Target",
+  "treefish", "Target",
+  "vermilion/canary", "Target",
+  "yellowtail/olive", "Target",
+  "yoy rockfish",  "Target"), nrow=2)
+speciescategory<-data.frame(t(speciescategory))
+names(speciescategory) <- c("Species","Target")
+speciescategory
+
+ReefcheckFISH_forAnalysis <- merge(ReefcheckFISH_forAnalysis,speciescategory,by="Species")  
+head(ReefcheckFISH_forAnalysis)
+
+#use small, medium, large data
+table(ReefcheckFISH_forAnalysis$SizeCategory)
+
+#replace sizes here... and remove remaining "see min_cm and max_cm"
+ReefcheckFISH_forAnalysis2<-ReefcheckFISH_forAnalysis %>% mutate(SizeCategory=replace(SizeCategory, Size_cm<15 & SizeCategory=="see min_cm and max_cm","Small"))%>% 
+  mutate(SizeCategory=replace(SizeCategory, Size_cm>=15 & Size_cm<=30 & SizeCategory=="see min_cm and max_cm","Medium"))%>%
+  mutate(SizeCategory=replace(SizeCategory, Size_cm>30 & SizeCategory=="see min_cm and max_cm","Large"))%>%
+  filter(SizeCategory!="see min_cm and max_cm")
+
+table(ReefcheckFISH_forAnalysis2$SizeCategory)
+
+
+###1. Santa Rosa
+AbundanceSRosa<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SRosa") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+AbundanceSRosa  
+
+MAbundanceSRosa <- AbundanceSRosa  %>% group_by(Year, site,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+MAbundanceSRosa
+
+ggplot(MAbundanceSRosa,aes(x=Year, y=meanA, group=site, color=site, shape=site))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)
+
+
+##use the small, medium, large details
+AbundanceSRosa<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SRosa" & SizeCategory=="Small") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+AbundanceSRosa  
+
+MAbundanceSRosa <- AbundanceSRosa  %>% group_by(Year, site,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+MAbundanceSRosa
+
+p1<-ggplot(MAbundanceSRosa,aes(x=Year, y=meanA, group=site, color=site, shape=site))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Small (size<15cm)")
+p1
+
+AbundanceSRosa<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SRosa" & SizeCategory=="Medium") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+MAbundanceSRosa <- AbundanceSRosa  %>% group_by(Year, site,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p2<-ggplot(MAbundanceSRosa,aes(x=Year, y=meanA, group=site, color=site, shape=site))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Medium (size=15-30cm)")
+
+AbundanceSRosa<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SRosa" & SizeCategory=="Large") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+MAbundanceSRosa <- AbundanceSRosa  %>% group_by(Year, site,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p3<-ggplot(MAbundanceSRosa,aes(x=Year, y=meanA, group=site, color=site, shape=site))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Large (size>30cm)")
+
+library(gridExtra)
+grid.arrange(p1,p2,p3,nrow=3)
+
+
+#2. Anacapa
+Head<-"Anacapa"
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="Anacapa" & !is.na(Amount)) %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+table(Abundance$site)
+
+MPAin<-c("Anacapa_SMR1","Anacapa_SMR2","Anacapa_SMR3")
+MPAout<-c("Anacapa_OUT")
+
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+
+ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+  
+  
+  #stat_compare_means(aes(group = MPA), label = "p.signif",label.y=10)+
+  geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle(Head)
+
+
+#compare_means(MAbundance,paired= TRUE)
+
+
+##use the small, medium, large details
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="Anacapa" & !is.na(Amount) & SizeCategory=="Small") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p1<-ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Small (size<15cm)")
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="Anacapa" & !is.na(Amount) & SizeCategory=="Medium") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p2<-ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Medium (size=15-30cm)")
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="Anacapa" & !is.na(Amount) & SizeCategory=="Large") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p3<-ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Large (size>30cm)")
+
+grid.arrange(p1,p2,p3,nrow=3,top=Head)
+
+
+#3. Santa Cruz
+Head<-"Santa Cruz"
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SCruz" & !is.na(Amount)) %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+table(Abundance$site)
+
+MPAin<-c("Scruz_Scorpion")
+MPAout<-c("Scruz_Out_Back1",  "Scruz_Out_Back2", "Scruz_Out_Front1", "Scruz_Out_Front2", "Scruz_Out_Front3")
+
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+
+ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+  
+  geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle(Head)
+
+
+##use the small, medium, large details
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SCruz" & !is.na(Amount) & SizeCategory=="Small") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p1<-ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Small (size<15cm)")
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SCruz" & !is.na(Amount) & SizeCategory=="Medium") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p2<-ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Medium (size=15-30cm)")
+Abundance<- ReefcheckFISH_forAnalysis2 %>% filter(Island=="SCruz" & !is.na(Amount) & SizeCategory=="Large") %>% 
+  group_by(Year, Transect, site,Target) %>% summarize(abundance=sum(Amount))
+Abundance<- Abundance %>% filter(site %in% c(MPAin,MPAout)) %>% mutate(MPA = ifelse(site %in% MPAin, "Inside", "Outside"))
+MAbundance <- Abundance  %>% group_by(Year, MPA,Target) %>% summarize(meanA=mean(abundance),sd=sd(abundance),ntrans=n())
+p3<-ggplot(MAbundance,aes(x=Year, y=meanA, color=MPA, shape=MPA))+
+  geom_errorbar(aes(ymin=meanA-sd,ymax=meanA+sd),width=.1)+geom_line()+geom_point(size=4)+
+  theme_minimal()+labs(y="Abundance")+theme(legend.title=element_blank())+facet_grid(.~Target)+ggtitle("Large (size>30cm)")
+
+grid.arrange(p1,p2,p3,nrow=3,top=Head)
+
+
+##############-------------------------------------------------------------------
+
+
 
 
 ###Data cleaning, analysis
@@ -245,16 +498,28 @@ speciescategory
 ReefcheckFISH_forAnalysis <- merge(ReefcheckFISH_forAnalysis,speciescategory,by="Species")  
 head(ReefcheckFISH_forAnalysis)
 
+#1st: Remove Amount = 0 or NA
+table(ReefcheckFISH_forAnalysis$Amount) #there are no NAs or Os
 
 #----THIS IS JUST FOR CHECKING WHICH OF THE SITES ARE MONITORED FREQUENTLY 
 #2006 to 2016
-ReefcheckFISH_forAnalysis2006<-ReefcheckFISH_forAnalysis[which(ReefcheckFISH_forAnalysis$Year==2010),]
+ReefcheckFISH_forAnalysis2006<-ReefcheckFISH_forAnalysis[which(ReefcheckFISH_forAnalysis$Year==2015),]
 table(ReefcheckFISH_forAnalysis2006$site,ReefcheckFISH_forAnalysis2006$Transect)
 #---Check all year
 #table(ReefcheckFISH_forAnalysis$site,ReefcheckFISH_forAnalysis$Transect,ReefcheckFISH_forAnalysis$Year)
 
 ##Now, I can isolate Anacapa first as it is one of the Islands that I can do meaningful analysis
 #ReefcheckFISH_forAnalysisAnacapa<-ReefcheckFISH_forAnalysis[which(ReefcheckFISH_forAnalysis$Island=="Anacapa"),]
+
+
+#Anacapa
+AbundanceAnacapa<- ReefcheckFISH_forAnalysis %>% filter(Island=="Anacapa", Target=="T") %>% 
+  group_by(Year, Transect, site) %>% summarize(abundance=sum(Amount))
+AbundanceAnacapa  
+
+ggplot(data=AbundanceAnacapa) +
+  geom_point(mapping=aes(x=Year, y=abundance, group=site, color=site))+
+  geom_smooth(mapping=aes(x=Year, y=abundance, group=site, color=site))
 
 
 
@@ -306,15 +571,33 @@ ggplot(data=AbundanceSCruz) +
   geom_smooth(mapping=aes(x=Year, y=abundance, group=site, color=site))
 
 ###TRY for Santa Rosa
-AbundanceSRosa<- ReefcheckFISH_forAnalysis %>% filter(Island=="SRosa", !is.na(Amount), Target=="T") %>% 
+AbundanceSRosa<- ReefcheckFISH_forAnalysis %>% filter(Island=="SRosa", Target=="T") %>% 
   group_by(Year, Transect, site) %>% summarize(abundance=sum(Amount))
 AbundanceSRosa  
 
-#AbundanceSRosa  %>%
+table(AbundanceSRosa$Year,AbundanceSRosa$Transect)
+
+#I will make a year-transect table
+#head(ReefcheckFISH_forAnalysis)
+#for unique(ReefcheckFISH_forAnalysis)
+#mytable<-
+
+
+
+
+
+#i will assume that there are 18 transects per survey
+MAbundanceSRosa <- AbundanceSRosa  %>% group_by(Year, site) %>% summarize(meanabundance=sum(abundance)/18)
+ggplot(data=MAbundanceSRosa) +
+  geom_point(mapping=aes(x=Year, y=meanabundance, group=site, color=site))
+
+
+AbundanceSRosa  %>% group_by(Year, site) %>% summarize(meanabundance=mean(abundance),ntransect=n())
 
 ggplot(data=AbundanceSRosa) +
   geom_point(mapping=aes(x=Year, y=abundance, group=site, color=site))+
   geom_smooth(mapping=aes(x=Year, y=abundance, group=site, color=site))
+
 
 
 
